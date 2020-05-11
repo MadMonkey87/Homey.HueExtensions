7c6d0005179fe3abd4fec7d262003d68c330b714
MadMonkey87 <myself@philippewechsler.ch>
2020-05-11 07:48:01 +0200

    set relative color teperature for lights and groups

--- ch.philippewechsler.hue/app.js
+++ ch.philippewechsler.hue/app.js
@@ -212,6 +212,70 @@ class HueExtensionsApp extends Homey.App {
 										})
 									});
 								});
+
+								let setGroupRelativeCtAction = new Homey.FlowCardAction('set_group_relative_ct');
+								setGroupRelativeCtAction
+									.register()
+									.registerRunListener(async ( args, state ) => {
+										const groupState = { ct_inc : Math.round(args.relative_increasement * 65534) };
+										return new Promise((resolve) => {
+											this.setGroupState(args.group.id, groupState, (error, result) => {
+												if (error) {
+													return this.error(error);
+												}
+												resolve(true);
+											})
+										});
+									})
+									.getArgument('group')
+									.registerAutocompleteListener(( query, args ) => {
+										return new Promise((resolve) => {
+											this.getGroupsList((error, groups) => {
+												if (error) {
+													return this.error(error);
+												}
+												let result = [{ name: 'All lights', id: '0'}];
+												Object.entries(groups).forEach(entry => {
+													const key = entry[0];
+													const group = entry[1];
+													result.push({name: group.name, id: key});
+												});
+												resolve(result);
+											})
+										});
+									});
+				
+									let setLightRelativeCtAction = new Homey.FlowCardAction('set_light_relative_ct');
+									setLightRelativeCtAction
+										.register()
+										.registerRunListener(async ( args, state ) => {
+											const lightState = { ct_inc : Math.round(args.relative_increasement * 65534) };
+											return new Promise((resolve) => {
+												this.setLightState(args.light.id, lightState, (error, result) => {
+													if (error) {
+														return this.error(error);
+													}
+													resolve(true);
+												})
+											});
+										})
+										.getArgument('light')
+										.registerAutocompleteListener(( query, args ) => {
+											return new Promise((resolve) => {
+												this.getLightsList((error, groups) => {
+													if (error) {
+														return this.error(error);
+													}
+													let result = [];
+													Object.entries(groups).forEach(entry => {
+														const key = entry[0];
+														const light = entry[1];
+														result.push({name: light.name,id: key});
+													});
+													resolve(result);
+												})
+											});
+										});
 	}
 
 	getLightState(device, callback) {
